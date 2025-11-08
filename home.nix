c37f1e24964e2a796bbd145e9f07583f6adce0b0
thenist <p717394@gmail.com>
2025-11-08 20:28:55 +0900

    Add Gnome Extensions.

--- home.nix
+++ home.nix
@@ -5,8 +5,9 @@
   home.homeDirectory = "/home/thenist";
   home.stateVersion = "25.05";
 
-  home.packages = [
-    pkgs.whitesur-gtk-theme
+  home.packages = with pkgs; [
+    gnomeExtensions.search-light
+    gnomeExtensions.blur-my-shell
   ];
 
   gtk = {
@@ -25,9 +26,17 @@
 
   home.pointerCursor = {
     name = "WhiteSur-cursors";
-    package = pkgs.whitesur-cursors;
     size = 24;
-
+    package = pkgs.whitesur-cursors;
     gtk.enable = true;
+  };
+
+  dconf.settings = {
+    "org/gnome/shell" = {
+      enabled-extensions = [
+        "search-light@icedman.github.com"
+        "blur-my-shell@aunetx"
+      ];
+    };
   };
 }
