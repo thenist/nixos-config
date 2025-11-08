c48f706c0c3b7131a534dc19e022f8243a2f0fd4
thenist <p717394@gmail.com>
2025-11-08 19:43:00 +0900

    Install WhiteSur theme

--- home.nix
+++ home.nix
@@ -4,4 +4,30 @@
   home.username = "thenist";
   home.homeDirectory = "/home/thenist";
   home.stateVersion = "25.05";
+
+  home.packages = [
+    pkgs.whitesur-gtk-theme
+  ];
+
+  gtk = {
+    enable = true;
+
+    theme = {
+      name = "WhiteSur-Dark";
+      package = pkgs.whitesur-gtk-theme;
+    };
+
+    iconTheme = {
+      name = "WhiteSur-dark";
+      package = pkgs.whitesur-icon-theme;
+    };
+  };
+
+  home.pointerCursor = {
+    name = "WhiteSur-cursors";
+    package = pkgs.whitesur-cursors;
+    size = 24;
+
+    gtk.enable = true;
+  };
 }
