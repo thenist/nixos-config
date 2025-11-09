f70b1e86bfee47fa8e2c9fadb0e88ff9e8e4c06b
thenist <p717394@gmail.com>
2025-11-09 23:42:19 +0900

    Install steam.

--- users/thenist/user.nix
+++ users/thenist/user.nix
@@ -1,6 +1,12 @@
 { config, pkgs, ... }:
 
 {
+  programs.steam = {
+    enable = true; # Master switch, already covered in installation
+    remotePlay.openFirewall = true;  # For Steam Remote Play
+    dedicatedServer.openFirewall = true; # For Source Dedicated Server hosting
+    # Other general flags if available can be set here.
+  };
   users.users.thenist = {
     isNormalUser = true;
     description = "thenist";
