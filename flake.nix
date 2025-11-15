db3dba3270c4f7b3051af42d3d1af1eb2b8b80c1
thenist <p717394@gmail.com>
2025-11-15 22:53:25 +0900

    Use nixos-hardware.

--- flake.nix
+++ flake.nix
@@ -4,13 +4,15 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, nixos-hardware, ... }@inputs: {
     # use "nixos", or your hostname as the name of the configuration
     # it's a better practice than "default" shown in the video
     nixosConfigurations.tondemo = nixpkgs.lib.nixosSystem {
@@ -20,6 +22,7 @@
         ./hosts/tondemo/configuration.nix
         ./hosts/tondemo/hardware-configuration.nix
         ./users/thenist/user.nix
+        nixos-hardware.nixosModules.lenovo-thinkpad-t480
         home-manager.nixosModules.home-manager
         {
           home-manager = {
